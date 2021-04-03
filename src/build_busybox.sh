c7bf737fb5cd5b36403794121c12d9049e02bd3c
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-03 11:10:29 +1100

    Finally fixed src/build_busybox.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/build_busybox.sh
+++ src/build_busybox.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
 
+EXE=$(realpath $0)
+
 if [ -z "$1" ]; then
     echo Usage: $0 [busybox-config]
     exit 1
@@ -14,7 +16,36 @@ mkdir -p rootfs
 ROOTFS=$(realpath rootfs)
 SYSROOT=$(realpath build_dir)
 
-cd dep/busybox
+cd dep/linux-pam/
+
+if [ ! -f ./configure ] || [ ./autogen.sh -nt ./configure ]; then
+    ./autogen.sh
+fi
+
+export CC=${SYSROOT}/usr/bin/musl-clang
+export CFLAGS="-march=native -mtune=native -Oz -flto"
+export LDFLAGS='-flto -fuse-ld=musl-clang-lld-static -Wl,--plugin-opt=O3,-O3,--gc-sections,--icf=safe'
+export PKG_CONFIG="${SYSROOT}/usr/bin/musl-pkg-config"
+
+if [ ! -f config.status ] || [ "$EXE" -nt config.status ]; then
+    ./configure --prefix=${SYSROOT}/usr/ \
+        --enable-static=yes \
+        --enable-shared=no \
+        --disable-pie \
+        --disable-doc \
+        --enable-db=no \
+        --disable-lckpwdf \
+        --disable-audit \
+        --disable-nis \
+        --disable-selinux \
+        --disable-econf \
+        --disable-nls
+fi
+
+make -j $(nproc)
+make install
+
+cd ../busybox
 
 # Use rsync to update .config only when necessary to avoid recompilation
 rsync -a $config .config
@@ -27,7 +58,7 @@ sed -i "s/CONFIG_SYSROOT=.*/CONFIG_SYSROOT=\"$CONFIG_SYSROOT\"/" .config
 
 #export CONFIG_SYSROOT="$SYSROOT"
 
-make #-j $(nproc)
-CONFIG_PREFIX="ROOTFS" make install
+make -j $(nproc)
+make install
 
-
+cp -r _install/* "$ROOTFS"
