513e92cf4772f93cabcd06f56f129189978a6d9f
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-04 00:04:44 +1100

    Add /init checking in src/build_initramfs.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/build_initramfs.sh
+++ src/build_initramfs.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -ex
 
 cd $(dirname $0)/rootfs/
+
+if [ ! -f init ]; then
+    echo rootfs/init does not exist!
+    exit 1
+fi
+
 find . | cpio --quiet -o --format='newc' | xz -C crc32 -9e -T0 >../boot/initramfs.img
