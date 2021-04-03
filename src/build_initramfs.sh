bdd397ba81a4a6c0556cc0c813707c5d62535614
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-03 20:28:22 +1100

    Fix src/build_initramfs.sh: Fix xz formatting
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/build_initramfs.sh
+++ src/build_initramfs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 
 cd $(dirname $0)/rootfs/
-find . | cpio -o --format='newc' | xz -9e -T0 >../boot/initramfs.img
+find . | cpio --quiet -o --format='newc' | xz -C crc32 -9e -T0 >../boot/initramfs.img
