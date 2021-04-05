f529c62d34f56b3c1f370c8eebbf93fae1e0b24f
Jiahao XU <Jiahao_XU@outlook.com>
2021-04-05 12:35:19 +1000

    Add file type checking on arg and use quoted variable expansion
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/run_in_vm.sh
+++ src/run_in_vm.sh
@@ -5,18 +5,31 @@ if [ -z "$1" ] || [ -z "$2" ]; then
     exit 1
 fi
 
+if ! file "$1" | grep -q 'Linux kernel'; then
+    echo "$1" should be a Linux kernel image
+    exit 1
+fi
+
+initramfs_file=$(file "$2")
+
+if (! echo "$initramfs_file" | grep -q 'compressed data') && (! echo "$initramfs_file" | grep cpio)
+then
+    echo "$2" should be a initramfs image
+    exit 1
+fi
+
 if [ -n "$3" ]; then
     ARCH="$3"
 else
     ARCH=$(arch)
 fi
 
-exec qemu-system-${ARCH} \
+exec qemu-system-"$ARCH" \
     -curses \
     -no-reboot \
     -enable-kvm \
     -cpu host \
     -m 512M \
     -smp $(nproc) \
-    -kernel $1 \
-    -initrd $2
+    -kernel "$1" \
+    -initrd "$2"
